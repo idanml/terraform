944e991c278149e890f2608c36107b610843a234
Idan <idanml@gmail.com>
2021-04-16 18:31:32 +0300

    add vnet and nsg

---
+++ providers.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+subscription_id    =    var.subscriptionid
+client_id          =    var.clientid
+tenant_id          =    var.tenantid
+client_secret      =    var.clientsecret
+features {}
+}
